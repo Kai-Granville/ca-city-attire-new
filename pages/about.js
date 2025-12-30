c8b81763a7352bd9d992f85fcd1ba1ed3d3143fa
Kai-Granville <161145259+Kai-Granville@users.noreply.github.com>
2025-12-30 19:21:35 +0000

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>
+        We curate products from trusted retailers. 
+        Purchases are made with third-party sellers.
+      </p>
+    </main>
+  );
+}
