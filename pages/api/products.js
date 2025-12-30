c486f6d454f0755d7610e9b0dd527677f3db7d59
Kai-Granville <161145259+Kai-Granville@users.noreply.github.com>
2025-12-30 19:25:04 +0000

    Update products.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,11 +1,5 @@
+import mockProducts from "../../data/mockProducts";
+
 export default function handler(req, res) {
-  res.status(200).json([
-    {
-      id: "1",
-      title: "Sample Product",
-      price: "£29.99",
-      image: "https://via.placeholder.com/300",
-      link: "#"
-    }
-  ]);
+  res.status(200).json(mockProducts);
 }
